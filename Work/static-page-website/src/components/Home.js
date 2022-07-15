333a4aef76f34ed9bdfabf328f47679e549fef97
Avin Wilson <avinwilson54@gmail.com>
2022-07-15 18:33:25 +0530

    Initial commit

---
+++ Work/static-page-website/src/components/Home.js
@@ -0,0 +1,10 @@
+ import React from "react";
+
+export default function Home() {
+    return (
+        <div className="home-page bg">
+            <h1 className="heading-style">Home</h1>
+            <p className="para-style lg-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et urna cursus dolor efficitur tincidunt. Phasellus molestie molestie justo ut pulvinar. Phasellus molestie dolor eget diam laoreet ultricies.</p>
+        </div>
+    );
+}
